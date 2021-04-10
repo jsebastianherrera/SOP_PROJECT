6fd5534849f3c283c53b1d3d1e4166364a7b6098
Honeypot <juansehgu@gmail.com>
2021-04-10 13:41:02 -0500

    Added makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+control:
+
+clean:
+		rm -f *.o *.h.gch control agente
---
+++ data.h
@@ -0,0 +1,53 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "reserva.h"
+#define MAX_RESERVARTION 20
+typedef struct data
+{
+    reserva reservation[MAX_RESERVARTION];
+    size_t size;
+} data;
+void write_pipe(int fd, void *buf, size_t size, char *pipe)
+{
+    int flag = 0, bytes;
+    do
+    {
+        fd = open(pipe, O_WRONLY);
+        if (fd == -1)
+        {
+            perror("pipe");
+            printf(" Se volvera a intentar despues\n");
+            sleep(5);
+        }
+        else
+            flag = 1;
+    } while (flag == 0);
+    bytes = write(fd, buf, size);
+    printf("Sent it:%d\n", bytes);
+    close(fd);
+}
+void read_pipe(int fd, void *buf, size_t size, char *pipe)
+{
+    int flag = 0, bytes;
+    do
+    {
+        fd = open(pipe, O_RDONLY);
+        if (fd == -1)
+        {
+            perror("pipe");
+            printf(" Se volvera a intentar despues\n");
+            sleep(5);
+        }
+        else
+            flag = 1;
+    } while (flag == 0);
+    bytes = read(fd, buf, size);
+    printf("Received it:%d\n", bytes);
+    close(fd);
+}
